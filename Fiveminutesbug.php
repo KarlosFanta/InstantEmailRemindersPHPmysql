213267f1d69b4f691b7b1ba481fa871067909722
KarlosFanta <leeuwenpatrol@gmail.com>
2015-05-21 20:16:59 +0200

    Update Fiveminutesbug.php

--- Fiveminutesbug.php
+++ Fiveminutesbug.php
@@ -6,23 +6,23 @@ date_default_timezone_set('Africa/Harare');
 echo "<br><b>5min bug<br>CHange last digit of Time1 to a 7 </b>";
 echo  "<br><br>";
 
-//$queryUU = "SELECT Time1 FROM eventsT";
+//$queryUU = "SELECT Time1 FROM eventsOnceOff";
 //SUBSTRING(that_column, 1, CHAR_LENGTH(that_column) - 2) AS that_column_trimmed
-//$queryUU = "UPDATE eventsT SET Time1 =concat(left(Time1,length(field) -5),'7') ";
+//$queryUU = "UPDATE eventsOnceOff SET Time1 =concat(left(Time1,length(field) -5),'7') ";
 
 /*
-$queryUU = "UPDATE eventsT IF (LEFT(Time1,4) = '0') THEN
+$queryUU = "UPDATE eventsOnceOff IF (LEFT(Time1,4) = '0') THEN
  SET Time1 = CONCAT( LEFT(Time1,4), '2')
 ELSEIF (LEFT(Time1,4) = '1') THEN
-UPDATE eventsT SET Time1 = 
+UPDATE eventsOnceOff SET Time1 = 
 CONCAT( LEFT(Time1,4), '2')
 ELSEIF (LEFT(Time1,4) = '8') THEN
-UPDATE eventsT SET Time1 = 
+UPDATE eventsOnceOff SET Time1 = 
 CONCAT( LEFT(Time1,4), '7')
-ELSE UPDATE eventsT SET Time1 = 
+ELSE UPDATE eventsOnceOff SET Time1 = 
 CONCAT( LEFT(Time1,4), '7')";
 
-$queryUU = "UPDATE eventsT 
+$queryUU = "UPDATE eventsOnceOff 
 SET Time1 = CONCAT( LEFT(Time1,4), '2') IF (RIGHT(Time1,1) = '0') 
 SET Time1 = CONCAT( LEFT(Time1,4), '2') IF (RIGHT(Time1,1) = '1') 
 SET Time1 = CONCAT( LEFT(Time1,4), '7') IF (RIGHT(Time1,1) = '3') 
@@ -33,14 +33,14 @@ SET Time1 = CONCAT( LEFT(Time1,4), '7') IF (RIGHT(Time1,1) = '8')
 SET Time1 = CONCAT( LEFT(Time1,4), '7') IF (RIGHT(Time1,1) = '9') 
 ";
 
-$queryUU = "UPDATE eventsT SET Time1 =CONCAT(
+$queryUU = "UPDATE eventsOnceOff SET Time1 =CONCAT(
                    REPLACE(
                       RIGHT(products_id,1), 'U', 'S'),      
                       SUBSTRING(products_id, 2, CHAR_LENGTH(products_id)
 ))";
 */
 
-$queryUU = "UPDATE eventsT SET Time1 = 
+$queryUU = "UPDATE eventsOnceOff SET Time1 = 
 (CONCAT( LEFT(Time1,4), '2')) where (RIGHT(Time1,1) = '0' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -49,7 +49,7 @@ else
 	echo 'Error NOT updated '.$queryUU;
 	
 	
-$queryUU = "UPDATE eventsT SET Time1 = 
+$queryUU = "UPDATE eventsOnceOff SET Time1 = 
 (CONCAT( LEFT(Time1,4), '2')) where (RIGHT(Time1,1) = '1' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -58,7 +58,7 @@ else
 	echo 'Error NOT updated '.$queryUU;
 		
 
-$queryUU = "UPDATE eventsT SET Time1 = 
+$queryUU = "UPDATE eventsOnceOff SET Time1 = 
 (CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '3' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -66,7 +66,7 @@ echo 'updated $queryUU '.$queryUU."<br>";
 else 
 	echo 'Error NOT updated '.$queryUU;
 		
-$queryUU = "UPDATE eventsT SET Time1 = 
+$queryUU = "UPDATE eventsOnceOff SET Time1 = 
 (CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '4' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -74,7 +74,7 @@ echo 'updated $queryUU '.$queryUU."<br>";
 else 
 	echo 'Error NOT updated '.$queryUU;
 		
-$queryUU = "UPDATE eventsT SET Time1 = 
+$queryUU = "UPDATE eventsOnceOff SET Time1 = 
 (CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '5' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -82,7 +82,7 @@ echo 'updated $queryUU '.$queryUU."<br>";
 else 
 	echo 'Error NOT updated '.$queryUU;
 		
-$queryUU = "UPDATE eventsT SET Time1 = 
+$queryUU = "UPDATE eventsOnceOff SET Time1 = 
 (CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '6' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -90,7 +90,7 @@ echo 'updated $queryUU '.$queryUU."<br>";
 else 
 	echo 'Error NOT updated '.$queryUU;
 		
-$queryUU = "UPDATE eventsT SET Time1 = 
+$queryUU = "UPDATE eventsOnceOff SET Time1 = 
 (CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '8' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -98,7 +98,7 @@ echo 'updated $queryUU '.$queryUU."<br>";
 else 
 	echo 'Error NOT updated '.$queryUU;
 		
-$queryUU = "UPDATE eventsT SET Time1 = 
+$queryUU = "UPDATE eventsOnceOff SET Time1 = 
 (CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '9' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -243,6 +243,81 @@ else
 	echo 'Error NOT updated '.$queryUU;
 		
 $queryUU = "UPDATE dailyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '9' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+
+
+
+
+
+
+
+
+
+$queryUU = "UPDATE yearlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '2')) where (RIGHT(Time1,1) = '0' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+	
+	
+$queryUU = "UPDATE yearlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '2')) where (RIGHT(Time1,1) = '1' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+
+$queryUU = "UPDATE yearlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '3' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE yearlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '4' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE yearlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '5' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE yearlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '6' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE yearlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '8' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE yearlyevents SET Time1 = 
 (CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '9' )";
 
 if (mysqli_query($DBConnect, $queryUU) === TRUE)    
@@ -251,4 +326,81 @@ else
 	echo 'Error NOT updated '.$queryUU;
 		
 			
+	
+
+
+
+
+
+$queryUU = "UPDATE monthlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '2')) where (RIGHT(Time1,1) = '0' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+	
+	
+$queryUU = "UPDATE monthlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '2')) where (RIGHT(Time1,1) = '1' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+
+$queryUU = "UPDATE monthlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '3' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE monthlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '4' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE monthlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '5' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE monthlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '6' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE monthlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '8' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+$queryUU = "UPDATE monthlyevents SET Time1 = 
+(CONCAT( LEFT(Time1,4), '7')) where (RIGHT(Time1,1) = '9' )";
+
+if (mysqli_query($DBConnect, $queryUU) === TRUE)    
+echo 'updated $queryUU '.$queryUU."<br>";
+else 
+	echo 'Error NOT updated '.$queryUU;
+		
+
+
+
+
+	
 ?>
