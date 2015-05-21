e7245a88b8e22acd0b3ee640ddae1ff337ec7573
KarlosFanta <leeuwenpatrol@gmail.com>
2015-05-21 20:54:32 +0200

    Update dailyReminders.php

--- dailyReminders.php
+++ dailyReminders.php
@@ -1,3 +1,10 @@
+<a href= 'http://localhost/ACSevents/viewRemallD.php'>viewRemallD.php</a>
+<BR>
+<BR><BR>
+<BR>
+<BR>
+<BR>
+<BR>
 
 <?php
 $DE2 = 'N';
@@ -6,6 +13,7 @@ $year = substr($todays_date, 0, 4);
 $month = substr($todays_date, 4, 2);
 $date = substr($todays_date, 6, 2);
 $trigger_date = date("Ymd", mktime (0,0,0,$month,$date,$year));
+
 $reminderDetails ='';
  require_once("DBcode.php");
 date_default_timezone_set('Africa/Harare');
@@ -15,28 +23,38 @@ echo "<br><b>the time is now:<br></b>";
 echo  date('H:i');
 $TimeNow = '';
 $TimeNow =  date('H:i');
+
+
+
+
 echo  "<br><br>";
 
 $queryW = "SELECT * FROM dailyevents WHERE Time1 = '$TimeNow' ORDER BY RemSubj ASC";
 echo $queryW;
+
+
 $rowcountD = 0;
 if ($resultW = mysqli_query($DBConnect, $queryW)) {
 echo "<table width='10' border='1'>\n";
 echo "<tr><th>id</th>";
 echo "<th>SRemSubj</th>";
 echo "<th>SRemBody</th>";
+
 echo "<th>Email</th>";
 echo "<th>Time1</th>";
 echo "</tr>\n";
 
 while ($row = mysqli_fetch_assoc($resultW)) {
+
 $rowcountD++;
+
 echo "<tr>";
 $id = $row["id"];
 echo "<th>".$row["id"]."</th>";
 echo "<th>".$row["RemSubj"]."</th>";
 $RemSubj = $row["RemSubj"];
 echo "<th>".$row["RemBody"]."</th>";
+
 echo "<th>".$row["Email"]."</th>";
 $DE2 = $row["Email"];
 echo "<th>".$row["Time1"]."</th>";
@@ -70,7 +88,7 @@ else
 
 
 
-/*//$queryM2 = "SELECT * FROM eventsT WHERE RemDate <= '$trigger_date' AND Sent = '' AND Time1 <= '$TimeNow' ";
+/*//$queryM2 = "SELECT * FROM eventsOnceOff WHERE RemDate <= '$trigger_date' AND Sent = '' AND Time1 <= '$TimeNow' ";
 $queryWUPD = "UPDATE dailyevents SET Sent = 'Sent'  WHERE  RemDate <= '$trigger_date' AND Sent = '' AND Time1 <= '$TimeNow'";
 echo "<br>".$queryWUPD;
 if (mysqli_query($DBConnect, $queryWUPD) === TRUE) {   
